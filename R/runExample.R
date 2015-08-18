feaf3842dc41658d8d58ed158db89cc3f146805e
= <=>
2015-08-18 15:32:37 +0100

    first check in

---
+++ R/runExample.R
@@ -0,0 +1,9 @@
+#' @export
+runExample <- function() {
+  appDir <- system.file("shiny-examples", "myapp", package = "elicitMS")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `elicitMS`.", call. = FALSE)
+}
+
+  shiny:runApp(appDir, display.mode = "normal")
+}
